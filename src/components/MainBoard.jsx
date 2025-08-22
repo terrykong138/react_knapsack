e6e4d2f2ecd0f30faebad83599c51acbb1b912aa
terrykong138 <terykongmail@yahoo.com>
2025-08-22 17:43:28 +0800

    revise  mainBoard

--- src/components/MainBoard.jsx
+++ src/components/MainBoard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState,useCallback } from 'react';
-import LogInForm from './LoginForm';
+import LogInForm from './LogInForm';
 import SignUpForm from './SignUpForm';
 // import NavbarHome from './NavbarHome';
 // import ContentCards from './ContentCards';
